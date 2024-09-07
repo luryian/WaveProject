9f4ab2d6c489e9ff2447066bcda2a3a4433df1b3
Maria Clara Freire Teles <mariaclarafreireteles@gmail.com>
2024-09-06 22:43:00 -0300

    add searchbar

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,20 +1,30 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import React from "react";
 import { useEffect, useState } from "react";
 import "./home.css";
 import { getProjetos } from "../services/firebase";
 
 export function Home() {
     const [projetos, setProjetos] = useState<any[]>([])
+    const [search, setSearch] = React.useState("");
+
 
     useEffect(() => {
         getProjetos(setProjetos)
     }, [])
     
 
+    const filteredProjetos = projetos.filter((elem) =>
+        elem.nome.toLowerCase().includes(search.toLowerCase()) // Comparação em letras minúsculas para evitar case sensitivity
+    );
+
+    console.log(search);
+
     return(
         <div className="body">
+            <input type="text" placeholder="Buscar projetos!" value={search} onChange={(e) => setSearch(e.target.value)}/>
             <div className="projetosList">
-                {projetos.map((elem) => (
+                {filteredProjetos.map((elem) => (
                     <div key={elem.documentId} className="projetoCard"> 
                         <h3> {elem.nome} </h3>
                         <p> {elem.descricao} </p>
