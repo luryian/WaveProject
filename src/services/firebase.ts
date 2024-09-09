014d422e2bcaa3cb005ed05442e116b6bc933196
Lucas Ryan <lucas.ryan2003lr@gmail.com>
2024-09-09 20:35:47 -0300

    -

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -15,7 +15,6 @@ const firebaseConfig = {
     measurementId: import.meta.env.VITE_APP_FIREBASE_MEASUREMENT_ID
 };
 
-
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
