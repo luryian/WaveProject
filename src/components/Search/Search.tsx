b7a15de8ae49d3f9281ad942ecb3a42adaa44082
Lemos <148251303+LemosSilveira@users.noreply.github.com>
2024-09-08 22:09:03 -0300

    project_details _first

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './Search.scss';
 
+
 interface SearchProps {
     search: string;
     setSearch: (value: string) => void;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,6 +7,8 @@ import logo from '../assets/logo.png';
 import trilhas from '../assets/trilhas.png';
 import DropdownFilter from '../components/DropdownFilter/DropdownFilter';
 import Footer from "../components/Footer/Footer"
+import cards from"../components/Cards/cards"
+
 // import Carousel from "../components/Carousel/Carousel"
 
 
@@ -28,6 +30,8 @@ export function Home() {
         elem.Codernador.toLowerCase().includes(search.toLowerCase()) // Comparação sem case sensitivity
       );
 
+      
+
     return(
         <div className="body">
             <div className="header">
@@ -44,16 +48,21 @@ export function Home() {
             <div className="navbar-container">
                 <Search search={search} setSearch={setSearch}/>
                 <DropdownFilter setTrilhaSelecionada={setTrilhaSelecionada} trilhaSelecionada={trilhaSelecionada} />
-            </div>
+            </div> 
             <div className="listaProjetos">
                 {filteredProjetos.map((elem) => (
                     <div className="card bg-base-100 w-96 shadow-xl">
+                        <div className="Button_SM" onClick={""}> 
+                            <a href="/details"><button className="Button_SM_details"> saiba mais</button></a>
+
+                        </div>
                         <div key={elem.documentId} className="card-body card-text">
                             <h2 className="card-title">{elem.nome}</h2>
                         </div>
                         <div className="card-actions justify-between items-center">
                             <p>{elem.Codernador}</p>
                             <div className="badge badge-outline">{elem.trilha}</div>
+                            
                         </div>
                     </div>
                 ))}
