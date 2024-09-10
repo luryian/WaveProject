7be98f17b29f467073a3e3d21093d005e32985a6
danielzcfer <nsei4648@gmail.com>
2024-09-10 15:29:51 -0300

    card_funcionando

--- src/components/Cards/cards.tsx
+++ src/components/Cards/cards.tsx
@@ -1,24 +1,10 @@
-import React, { useState } from "react";
-import { doc, getDoc } from "firebase/firestore"; // Para buscar um documento específico do Firestore
-import { db } from "./src\services\firebase.ts"; // Sua configuração do Firestore
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../../services/firebase.ts";
 
-const Card = ({ card }) => {
-  const [details, setDetails] = useState(null); // Estado para armazenar os detalhes
-  const [loading, setLoading] = useState(false); // Estado de carregamento
+export async function getProject(id: string) {
+  const projectRef = doc(db, 'projetos', id)
 
-  // Função para buscar detalhes do card
-  const fetchDetails = async () => {
-    setLoading(true); // Inicia o carregamento
-    const docRef = doc(db, "projetos", card.id); // Referência ao documento específico
-    const docSnap = await getDoc(docRef);
+  const project = await getDoc(projectRef)
 
-    setDetails(docSnap.data());
-
-    setLoading(false)
-
-    console.log(details)
-  }
-
-  
-
+  return project.data()
 }
