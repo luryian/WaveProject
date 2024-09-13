b150b54990daacab0edcf4efcc2ee2139a3878fc
Lucas Ryan <lucas.ryan2003lr@gmail.com>
2024-09-12 21:45:49 -0300

    ajuste de slider

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,57 +1,29 @@
-import { useState, useEffect } from 'react';
-import data from './CarouselData';
-import './Carousel.scss';
+import React from 'react';
+import { Carousel } from 'antd';
 
-export default function Carousel() {
-    const [currentSlide, setCurrentSlide] = useState(0);
-    const intervalTime = 3000;
+const contentStyle: React.CSSProperties = {
+  height: '400px',
+  color: '#fff',
+  lineHeight: '160px',
+  textAlign: 'center',
+  background: '#364d79',
+};
 
-    const nextSlide = () => {
-        setCurrentSlide((prevIndex) =>
-            prevIndex === data.length - 1 ? 0 : prevIndex + 1
-        );
-    };
+const Carrossel: React.FC = () => (
+  <Carousel autoplay>
+    <div>
+      <h3 style={contentStyle}>1</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>2</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>3</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>4</h3>
+    </div>
+  </Carousel>
+);
 
-    const prevSlide = () => {
-        setCurrentSlide((prevIndex) =>
-            prevIndex === 0 ? data.length - 1 : prevIndex - 1
-        );
-    };
-
-    useEffect(() => {
-        const slideInterval = setInterval(nextSlide, intervalTime);
-        return () => clearInterval(slideInterval);
-    }, []);
-
-    return (
-        <div className="carousel">
-            {data.map((slide, index) => {
-                let className = 'carousel-item';
-
-                if (index === currentSlide) {
-                    className += ' active';
-                } else if (index === (currentSlide === 0 ? data.length - 1 : currentSlide - 1)) {
-                    className += ' prev';
-                } else if (index === (currentSlide === data.length - 1 ? 0 : currentSlide + 1)) {
-                    className += ' next';
-                }
-
-                return (
-                    <div
-                        key={slide.id}
-                        className={className}
-                    >
-                        <img
-                            src={slide.image}
-                            alt={`Slide ${slide.id}`}
-                        />
-                        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                            <button onClick={prevSlide} className="btn btn-circle">❮</button>
-                            <button onClick={nextSlide} className="btn btn-circle">❯</button>
-                        </div>
-                    </div>
-                );
-            })}
-        </div>
-    );
-}
+export default Carrossel;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -11,7 +11,7 @@ import { getProjetos } from "../services/firebase";
 import Footer from "../components/Footer/Footer"
 import { getAuth } from "firebase/auth"; 
 
-// import Carousel from "../components/Carousel/Carousel"
+ import Carousel from "../components/Carousel/Carousel"
 
 export interface Project {
     Carga: string
@@ -58,35 +58,39 @@ export function Home() {
                 <img src={logo} alt="logo" className="logo" />
                 <h3> Página de Admin </h3>
             </div>
-
-            
-            <div className="trilhas-container">
-                <img src={trilhas} alt="trilha-design" />
-                <img src={trilhas} alt="trilha-audiovisual" />
-                <img src={trilhas} alt="trilha-programacao" />
-                <img src={trilhas} alt="trilha-jogos" />
+            <div className="carrossel">
+                <Carousel></Carousel>
             </div>
-            <div className="navbar-container">
-                <Search search={search} setSearch={setSearch}/>
-                <DropdownFilter setTrilhaSelecionada={setTrilhaSelecionada} trilhaSelecionada={trilhaSelecionada} />
-            </div> 
-            <div className="listaProjetos">
-                {filteredProjetos.map((elem) => (
-                    <div className="card bg-base-100 w-96 shadow-xl">
-                        <div className="Button_SM" onClick={() => getProject(elem.documentId)}> 
-                            <a href={`/details/${elem.documentId}`}><button className="Button_SM_details"> saiba mais</button></a>
 
+            <div className="body-container">
+                <div className="trilhas-container">
+                    <img src={trilhas} alt="trilha-design" />
+                    <img src={trilhas} alt="trilha-audiovisual" />
+                    <img src={trilhas} alt="trilha-programacao" />
+                    <img src={trilhas} alt="trilha-jogos" />
+                </div>
+                <div className="navbar-container">
+                    <Search search={search} setSearch={setSearch}/>
+                    <DropdownFilter setTrilhaSelecionada={setTrilhaSelecionada} trilhaSelecionada={trilhaSelecionada} />
+                </div> 
+                <div className="listaProjetos">
+                    {filteredProjetos.map((elem) => (
+                        <div className="card bg-base-100 w-96 shadow-xl" key={elem.documentId}>
+                            <div className="Button_SM" onClick={() => getProject(elem.documentId)}> 
+                                <a href={`/details/${elem.documentId}`}><button className="Button_SM_details"> saiba mais</button></a>
+
+                            </div>
+                            <div key={elem.documentId} className="card-body card-text">
+                                <h2 className="card-title">{elem.nome}</h2>
+                            </div>
+                            <div className="card-actions justify-between items-center">
+                                <p>{elem.Codernador}</p>
+                                <div className="badge badge-outline">{elem.trilha}</div>
+                                
+                            </div>
                         </div>
-                        <div key={elem.documentId} className="card-body card-text">
-                            <h2 className="card-title">{elem.nome}</h2>
-                        </div>
-                        <div className="card-actions justify-between items-center">
-                            <p>{elem.Codernador}</p>
-                            <div className="badge badge-outline">{elem.trilha}</div>
-                            
-                        </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
             <Footer />
         </div>
@@ -114,7 +118,7 @@ export function Home() {
             <div className="listaProjetos">
                 {filteredProjetos.map((elem) => (
                     <div className="card bg-base-100 w-96 shadow-xl">
-                        <div className="Button_SM" onClick={() => getProject(elem.documentId)}> 
+                        <div className="Button_SM" onClick={() => getProject(elem.documentId)} key={elem.documentId}> 
                             <a href={`/details/${elem.documentId}`}><button className="Button_SM_details"> saiba mais</button></a>
                         </div>
                         <div key={elem.documentId} className="card-body card-text">
