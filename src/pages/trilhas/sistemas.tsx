f01d351142f56cc4588a9052ba1ba81c8e745956
Lemos <148251303+LemosSilveira@users.noreply.github.com>
2024-09-27 12:33:13 -0300

    commit trilhas alta

--- src/pages/trilhas/sistemas.tsx
+++ src/pages/trilhas/sistemas.tsx
@@ -8,6 +8,9 @@ import DropdownFilter from '../../components/DropdownFilter/DropdownFilter';
 import Search from "../../components/Search/Search";
 import { Slider } from "../../components/Slider/Slider";
 import Footer from "../../components/Footer/Footer";
+import FilterFeedback from "../../components/FilterFeedback/FilterFeedback";
+import { getProject } from "../../components/Cards/cards";
+import { getProjetos, logout } from "../../services/firebase";
 
 
 export function Sistemas(){ 
@@ -15,13 +18,17 @@ export function Sistemas(){
     const [trilhaPage] = useState<Project[]>([])
     const [search, setSearch] = useState("");
     const [trilhaSelecionada, setTrilhaSelecionada] = useState<string | null>(null);
+    const [aplicacaoSelecionada, setAplicacaoSelecionada] = useState<string | null>(null);
+    const [projetos, setProjetos] = useState<Project[]>([]) 
 
     useEffect (() => {
-        getTrilha('design',trilhaPage)
-        console.log('é pra exucuutar uma vez')
+        if (projetos.length === 0) {
+            getProjetos(setProjetos)
+            console.log('é pra exucuutar uma vez')
+          
+        }
         
-    })
-
+    })  
    
 
     console.log(trilhaPage)
@@ -42,15 +49,55 @@ export function Sistemas(){
             <img src={ilha_Sistemas} alt="ilha_audivisual" className="asset-ilhaAudivisual" />
            </div>
 
-           <main>
+           
             <div>
                 <h1 className="title-audivisual"> Projetos de Pesquisa</h1>
                 <div className="navbar-container">
                     <Search search={search} setSearch={setSearch}/>
                 </div> 
             </div>
-           </main>
-           <Footer/>
+           
+            <div className="body">
+                    
+                    <div className="body-container">
+                        <div className="projetos-container">
+                            <div className="projetos-title">
+                            </div>
+                            <div className="navbar-container">
+                                <Search search={search} setSearch={setSearch}/>
+                            </div>
+                            <div className="filter-feedback">
+                                <FilterFeedback trilhaSelecionada={trilhaSelecionada} aplicacaoSelecionada={aplicacaoSelecionada}/>
+                            </div>
+                            <div className="filter-projetos">
+                                <div className="left-filter">
+                                    <h3>Filtros</h3>
+                                    <DropdownFilter setTrilhaSelecionada={setTrilhaSelecionada} trilhaSelecionada={trilhaSelecionada} />
+                                    <Slider/>
+                                </div>
+                                <div className="listaProjetos">
+                                    {projetos.map((elem) => (
+                                        <a href={`/details/${elem.documentId}`}>
+                                            <div className="card bg-base-100 w-96 shadow-xl" key={elem.documentId}>
+                                                <div className="Button_SM" onClick={() => getProject(elem.documentId)}> 
+                                                </div>
+                                                <div key={elem.documentId} className="card-body card-text">
+                                                    <h2 className="card-title">{elem.nome}</h2>
+                                                </div>
+                                                <div className="card-actions justify-between items-center">
+                                                    <p>{elem.Codernador}</p>
+                                                    <div className="badge badge-outline">{elem.trilha}</div>
+                                                    
+                                                </div>
+                                            </div>
+                                        </a>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <Footer />
+                    </div>
         </div>
         
         
